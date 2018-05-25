1bfa11f4d5a1462eb0224aed7de4f831c967382e
wcy <chuyiwu@foxmail.com>
2018-05-25 17:59:34 +0800

    add login

--- education_system_a/infoSystem/views.py
+++ education_system_a/infoSystem/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
+
 def login(request):
-    return render(request, 'login.html',{})
+    if request.method!='POST'：
+        return render(request,'login.html',{})
+    if type=='STUDENT':
+        try:
+            user=Student.objects.get(stu_ID=request.POST['name'])
+            render(request,'login.html',{'error':{'msg':'好嘛'}})
+        except:
+            render(request,'login.html',{'error':{'msg':'妈耶'}})
+    else if type=='TEACHER':
+        try:
+            user=Student.objects.get(ins_ID=request.POST['name'])
+    else if type=='EDUCATION MANAGER':
+        try:
+            user=Student.objects.get(Mana_ID=request.POST['name'])
+    else if type=='SYSTEM MANAGER':
+        try:
+            user=Student.objects.get(sys_ID=request.POST['name'])
+
+

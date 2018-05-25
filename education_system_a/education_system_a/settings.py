cd69709bc98a868fceac290cd5c0a7fd99e93327
wcy <chuyiwu@foxmail.com>
2018-05-25 14:42:02 +0800

    modify setting and db files

--- education_system_a/education_system_a/settings.py
+++ education_system_a/education_system_a/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'education_system_a.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [templates],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

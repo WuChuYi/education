336f699ba32c5e80ddf3842b2204cd0763a85082
wcy <chuyiwu@foxmail.com>
2018-05-25 19:08:08 +0800

    csrf problem

--- education_system_a/infoSystem/views.py
+++ education_system_a/infoSystem/views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import render
-
+from django.views.decorators.csrf import csrf_exempt
 # Create your views here.
 
+@csrf_exempt
 def login(request):
     if request.method!='POST':
-        return render(request,'login.html',{})
-    if type=='STUDENT':
+        return render(request,'login.html',{ 'error': ' ' })
+    type=request.POST['type']
+    if type=='student':
         try:
-            user=Student.objects.get(stu_ID=request.POST['name'])
-            render(request,'login.html',{'error':{'msg':'好嘛'}})
+            user=Student.objects.get(stu_ID=request.POST['id'])
+            print("aaa")
+            return render(request,'login.html',{'error':'好嘛'})
         except:
-            render(request,'login.html',{'error':{'msg':'妈耶'}})
-    elif type=='TEACHER':
-        try:
-            user=Student.objects.get(ins_ID=request.POST['name'])
-    elif type=='EDUCATION MANAGER':
-        try:
-            user=Student.objects.get(Mana_ID=request.POST['name'])
-    elif type=='SYSTEM MANAGER':
-        try:
-            user=Student.objects.get(sys_ID=request.POST['name'])
+            return render(request,'login.html',{'error':'妈耶'})
+    elif type=='teacher':
+        user=Student.objects.get(ins_ID=request.POST['id'])
+    elif type=='education manager':
+        user=Student.objects.get(Mana_ID=request.POST['id'])
+    elif type=='system manager':
+        user=Student.objects.get(sys_ID=request.POST['id'])
 
 

5a9f431e22f61b27b730fafba521e78b045eec81
wcy <chuyiwu@foxmail.com>
2018-06-01 19:19:24 +0800

    add black list

--- education_system_a/infoSystem/admin.py
+++ education_system_a/infoSystem/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Student, Instructor, Course, Edu_manager, Sys_manager, Ins_course
+from .models import Student, Instructor, Course, Edu_manager, Sys_manager, Ins_course, Blacklist
 admin.site.register(Student)
 admin.site.register(Instructor)
 admin.site.register(Course)
 admin.site.register(Edu_manager)
 admin.site.register(Sys_manager)
 admin.site.register(Ins_course)
+admin.site.register(Blacklist)
